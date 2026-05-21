e19678651cf3afb1614037274b93f4a79b60dd1e
ilfrich <das-peter@gmx.de>
2026-05-21 15:47:17 +1000

    Fixed setup py including readme and pushed new version

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="solsticeai",
-      version="1.0.0",
+      version="1.0.1",
+      description="Solstice AI - Client Implementation to Access Lakeside Services",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/solstice-ai/py-solstice-client",
+      author="Peter Ilfrich",
+      author_email="peter@solstice-ai.com",
+      license="Apache-2.0",
       packages=[
           "solsticeai"
       ],
@@ -10,5 +20,8 @@ setup(name="solsticeai",
           "pandas",
           "numpy",
           "pytz",
+      ],
+      tests_require=[
+        "pytest",
       ],
       zip_safe=False)
